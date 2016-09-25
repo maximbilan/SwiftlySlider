98f9b536afec7820f0c3fa7ef2c145bdca6f2bd7
Maxim Bilan <maximb.mail@gmail.com>
2016-09-25 09:36:11 +0300

    Converted code to Swift 3

--- SwiftlySlider/Sources/SwiftlySlider.swift
+++ SwiftlySlider/Sources/SwiftlySlider.swift
@@ -9,23 +9,23 @@
 import UIKit
 
 public protocol SwiftlySliderDelegate : class {
-	func swiftlySliderValueChanged(value: Int)
+	func swiftlySliderValueChanged(_ value: Int)
 }
 
-public class SwiftlySlider: UIView {
+open class SwiftlySlider: UIView {
 	
 	// MARK: - Direction
 	
 	public enum PickerDirection: Int {
-		case Horizontal
-		case Vertical
+		case horizontal
+		case vertical
 	}
 	
 	// MARK: - Public properties
 	
-	public weak var delegate: SwiftlySliderDelegate!
-	public var direction: PickerDirection = .Horizontal
-	public var currentValue: Int {
+	open weak var delegate: SwiftlySliderDelegate!
+	open var direction: PickerDirection = .horizontal
+	open var currentValue: Int {
 		get {
 			return value
 		}
@@ -43,47 +43,47 @@ public class SwiftlySlider: UIView {
 			setNeedsDisplay()
 		}
 	}
-	public var minValue: Int = 0
-	public var maxValue: Int = 20
-	public var normalValue: Int = 0
+	open var minValue: Int = 0
+	open var maxValue: Int = 20
+	open var normalValue: Int = 0
 	
 	// MARK: - Additional public properties
 	
-	public var labelFontColor: UIColor = UIColor.whiteColor()
-	public var labelBackgroundColor: UIColor = UIColor.blackColor()
-	public var labelFont = UIFont(name: "Helvetica Neue", size: 12)
-	public var bgColor: UIColor = UIColor.whiteColor()
-	public var bgCornerRadius: CGFloat = 30
-	public var barColor: UIColor = UIColor.grayColor()
-	public var sliderImage: UIImage?
-	public var sliderImageOffset: CGPoint = CGPointZero
-	public var sliderSize: CGSize = CGSizeZero
-	public var useNormalIndicator = false
+	open var labelFontColor: UIColor = UIColor.white
+	open var labelBackgroundColor: UIColor = UIColor.black
+	open var labelFont = UIFont(name: "Helvetica Neue", size: 12)
+	open var bgColor: UIColor = UIColor.white
+	open var bgCornerRadius: CGFloat = 30
+	open var barColor: UIColor = UIColor.gray
+	open var sliderImage: UIImage?
+	open var sliderImageOffset: CGPoint = CGPoint.zero
+	open var sliderSize: CGSize = CGSize.zero
+	open var useNormalIndicator = false
 	
 	// MARK: - Private properties
 	
-	private var value: Int = 0
-	private var image: UIImage!
-	private var currentSelectionY: CGFloat = 0
-	private var currentSelectionX: CGFloat = 0
+	fileprivate var value: Int = 0
+	fileprivate var image: UIImage!
+	fileprivate var currentSelectionY: CGFloat = 0
+	fileprivate var currentSelectionX: CGFloat = 0
 	
 	// MARK: - Initialization
 	
 	required public init?(coder aDecoder: NSCoder) {
 		super.init(coder: aDecoder)
 		
-		self.backgroundColor = UIColor.clearColor()
+		self.backgroundColor = UIColor.clear
 		value = minValue
 	}
 	
 	override public init(frame: CGRect) {
 		super.init(frame: frame)
 		
-		self.backgroundColor = UIColor.clearColor()
+		self.backgroundColor = UIColor.clear
 		value = minValue
 	}
 	
-	override public func layoutSubviews() {
+	override open func layoutSubviews() {
 		super.layoutSubviews()
 		
 		update()
@@ -91,17 +91,17 @@ public class SwiftlySlider: UIView {
 	
 	// MARK: - Prerendering
 	
-	func generateHUEImage(size: CGSize) -> UIImage {
+	func generateHUEImage(_ size: CGSize) -> UIImage {
 		
-		let rect = CGRectMake(0, 0, size.width, size.height)
-		var barRect = CGRectNull
-		if direction == .Horizontal {
+		let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+		var barRect = CGRect.null
+		if direction == .horizontal {
 			let barHeight = size.height * 0.2
-			barRect = CGRectMake(0, size.height * 0.5 - barHeight * 0.5, size.width, barHeight)
+			barRect = CGRect(x: 0, y: size.height * 0.5 - barHeight * 0.5, width: size.width, height: barHeight)
 		}
 		else {
 			let barWidth = size.width * 0.2
-			barRect = CGRectMake(size.width * 0.5 - barWidth * 0.5, 0, barWidth, size.height)
+			barRect = CGRect(x: size.width * 0.5 - barWidth * 0.5, y: 0, width: barWidth, height: size.height)
 		}
 		
 		UIGraphicsBeginImageContextWithOptions(size, false, 0)
@@ -114,8 +114,8 @@ public class SwiftlySlider: UIView {
 		let context = UIGraphicsGetCurrentContext()
 		barColor.set()
 		
-		CGContextSetFillColor(context!, CGColorGetComponents(barColor.CGColor))
-		CGContextFillRect(context!, barRect)
+		context!.setFillColor(barColor.cgColor.components!)
+		context!.fill(barRect)
 		
 		let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
 		UIGraphicsEndImageContext()
@@ -126,10 +126,10 @@ public class SwiftlySlider: UIView {
 	
 	func update() {
 		
-		let offset = (direction == .Horizontal ? self.frame.size.height : self.frame.size.width)
+		let offset = (direction == .horizontal ? self.frame.size.height : self.frame.size.width)
 		let halfOffset = offset * 0.5
 		var size = self.frame.size
-		if direction == .Horizontal {
+		if direction == .horizontal {
 			size.width -= offset
 		}
 		else {
@@ -144,51 +144,51 @@ public class SwiftlySlider: UIView {
 	
 	// MARK: - Drawing
 	
-	override public func drawRect(rect: CGRect) {
-		super.drawRect(rect)
+	override open func draw(_ rect: CGRect) {
+		super.draw(rect)
 		
 		let context = UIGraphicsGetCurrentContext()
 		
 		if useNormalIndicator {
-			var barRect = CGRectNull
-			if direction == .Horizontal {
+			var barRect = CGRect.null
+			if direction == .horizontal {
 				let barHeight = rect.size.height * 0.2
 				
-				barRect = CGRectMake(0, rect.size.height * 0.5 - barHeight * 0.5, rect.size.width, barHeight)
+				barRect = CGRect(x: 0, y: rect.size.height * 0.5 - barHeight * 0.5, width: rect.size.width, height: barHeight)
 			}
 			else {
 				let barWidth = rect.size.width * 0.2
-				barRect = CGRectMake(rect.size.width * 0.5 - barWidth * 0.5, 0, barWidth, rect.size.height)
+				barRect = CGRect(x: rect.size.width * 0.5 - barWidth * 0.5, y: 0, width: barWidth, height: rect.size.height)
 			}
 			
-			let offset = (direction == .Horizontal ? self.frame.size.height : self.frame.size.width)
+			let offset = (direction == .horizontal ? self.frame.size.height : self.frame.size.width)
 			let halfOffset = offset * 0.5
 			var size = self.frame.size
 			let trianglePath = UIBezierPath()
-			if direction == .Horizontal {
+			if direction == .horizontal {
 				size.width -= offset
 				let triangleHeight = rect.size.height * 0.3
 				let triangleX = ((CGFloat(normalValue - minValue) * (size.width)) / CGFloat(maxValue - minValue)) + halfOffset
-				trianglePath.moveToPoint(CGPoint(x: triangleX, y: barRect.origin.y))
-				trianglePath.addLineToPoint(CGPoint(x: triangleX - triangleHeight * 0.5, y: barRect.origin.y - triangleHeight))
-				trianglePath.addLineToPoint(CGPoint(x: triangleX + triangleHeight * 0.5, y: barRect.origin.y - triangleHeight))
-				trianglePath.addLineToPoint(CGPoint(x: triangleX, y: barRect.origin.y))
+				trianglePath.move(to: CGPoint(x: triangleX, y: barRect.origin.y))
+				trianglePath.addLine(to: CGPoint(x: triangleX - triangleHeight * 0.5, y: barRect.origin.y - triangleHeight))
+				trianglePath.addLine(to: CGPoint(x: triangleX + triangleHeight * 0.5, y: barRect.origin.y - triangleHeight))
+				trianglePath.addLine(to: CGPoint(x: triangleX, y: barRect.origin.y))
 			}
 			else {
 				size.height -= offset
 				let triangleWidth = rect.size.width * 0.3
 				let triangleY = ((CGFloat(normalValue - minValue) * (size.height)) / CGFloat(maxValue - minValue)) + halfOffset
-				trianglePath.moveToPoint(CGPoint(x: barRect.origin.x, y: triangleY))
-				trianglePath.addLineToPoint(CGPoint(x: barRect.origin.x - triangleWidth, y: triangleY - triangleWidth * 0.5))
-				trianglePath.addLineToPoint(CGPoint(x: barRect.origin.x - triangleWidth, y: triangleY + triangleWidth * 0.5))
-				trianglePath.addLineToPoint(CGPoint(x: barRect.origin.x, y: triangleY))
+				trianglePath.move(to: CGPoint(x: barRect.origin.x, y: triangleY))
+				trianglePath.addLine(to: CGPoint(x: barRect.origin.x - triangleWidth, y: triangleY - triangleWidth * 0.5))
+				trianglePath.addLine(to: CGPoint(x: barRect.origin.x - triangleWidth, y: triangleY + triangleWidth * 0.5))
+				trianglePath.addLine(to: CGPoint(x: barRect.origin.x, y: triangleY))
 			}
 			
-			CGContextSetFillColorWithColor(context!, barColor.CGColor)
+			context!.setFillColor(barColor.cgColor)
 			trianglePath.fill()
 		}
 		
-		let radius = (direction == .Horizontal ? self.frame.size.height : self.frame.size.width)
+		let radius = (direction == .horizontal ? self.frame.size.height : self.frame.size.width)
 		let halfRadius = radius * 0.5
 		var circleX = currentSelectionX - halfRadius
 		var circleY = currentSelectionY - halfRadius
@@ -205,12 +205,12 @@ public class SwiftlySlider: UIView {
 			circleY = 0
 		}
 		
-		let circleRect = (direction == .Horizontal ? CGRectMake(circleX, 0, radius, radius) : CGRectMake(0, circleY, radius, radius))
+		let circleRect = (direction == .horizontal ? CGRect(x: circleX, y: 0, width: radius, height: radius) : CGRect(x: 0, y: circleY, width: radius, height: radius))
 		let circleColor = labelBackgroundColor
 		var imageRect = rect
 		
 		if image != nil {
-			if direction == .Horizontal {
+			if direction == .horizontal {
 				imageRect.size.width -= radius
 				imageRect.origin.x += halfRadius
 			}
@@ -218,65 +218,65 @@ public class SwiftlySlider: UIView {
 				imageRect.size.height -= radius
 				imageRect.origin.y += halfRadius
 			}
-			image.drawInRect(imageRect)
+			image.draw(in: imageRect)
 		}
 		
 		if let image = sliderImage {
-			CGContextTranslateCTM(context!, 0, imageRect.size.height)
-			CGContextScaleCTM(context!, 1.0, -1.0)
-			CGContextDrawImage(context!, (sliderSize != CGSizeZero ? CGRectMake(circleRect.origin.x + sliderImageOffset.x, radius * 0.5 - sliderSize.height * 0.5 + sliderImageOffset.y, sliderSize.width, sliderSize.height) : circleRect), image.CGImage!)
+			context!.translateBy(x: 0, y: imageRect.size.height)
+			context!.scaleBy(x: 1.0, y: -1.0)
+			context!.draw(image.cgImage!, in: (sliderSize != CGSize.zero ? CGRect(x: circleRect.origin.x + sliderImageOffset.x, y: radius * 0.5 - sliderSize.height * 0.5 + sliderImageOffset.y, width: sliderSize.width, height: sliderSize.height) : circleRect))
 		}
 		else {
 			circleColor.set()
-			CGContextAddEllipseInRect(context!, circleRect)
-			CGContextSetFillColor(context!, CGColorGetComponents(circleColor.CGColor))
-			CGContextFillPath(context!)
-			CGContextStrokePath(context!)
+			context!.addEllipse(in: circleRect)
+			context!.setFillColor(circleColor.cgColor.components!)
+			context!.fillPath()
+			context!.strokePath()
 			
 			let textParagraphStyle = NSMutableParagraphStyle()
-			textParagraphStyle.alignment = .Center
+			textParagraphStyle.alignment = .center
 			
 			let attributes: NSDictionary = [NSForegroundColorAttributeName: labelFontColor,
 			                                NSParagraphStyleAttributeName: textParagraphStyle,
 			                                NSFontAttributeName: labelFont!]
 			
-			let text: NSString = "\(value)"
+			let text: NSString = "\(value)" as NSString
 			var textRect = circleRect
 			textRect.origin.y += (textRect.size.height - (labelFont?.lineHeight)!) * 0.5
-			text.drawInRect(textRect, withAttributes: attributes as? [String : AnyObject])
+			text.draw(in: textRect, withAttributes: attributes as? [String : AnyObject])
 		}
 	}
 	
 	// MARK: - Touch events
 	
-	override public func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+	override open func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 		let touch: AnyObject? = touches.first
-		let point = touch!.locationInView(self)
+		let point = touch!.location(in: self)
 		handleTouch(point)
 	}
 	
-	override public func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+	override open func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
 		let touch: AnyObject? = touches.first
-		let point = touch!.locationInView(self)
+		let point = touch!.location(in: self)
 		handleTouch(point)
 	}
 	
-	override public func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+	override open func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
 		let touch: AnyObject? = touches.first
-		let point = touch!.locationInView(self)
+		let point = touch!.location(in: self)
 		handleTouch(point)
 	}
 	
-	override public func touchesCancelled(touches: Set<UITouch>, withEvent event: UIEvent?) {
+	override open func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
 	}
 	
 	// MARK: - Touch handling
 	
-	func handleTouch(touchPoint: CGPoint) {
+	func handleTouch(_ touchPoint: CGPoint) {
 		currentSelectionX = touchPoint.x
 		currentSelectionY = touchPoint.y
 		
-		let offset = (direction == .Horizontal ? self.frame.size.height : self.frame.size.width)
+		let offset = (direction == .horizontal ? self.frame.size.height : self.frame.size.width)
 		let halfOffset = offset * 0.5
 		if currentSelectionX < halfOffset {
 			currentSelectionX = halfOffset
@@ -291,7 +291,7 @@ public class SwiftlySlider: UIView {
 			currentSelectionY = self.frame.size.height - halfOffset
 		}
 		
-		let percent = (direction == .Horizontal ? CGFloat((currentSelectionX - halfOffset) / (self.frame.size.width - offset))
+		let percent = (direction == .horizontal ? CGFloat((currentSelectionX - halfOffset) / (self.frame.size.width - offset))
 			: CGFloat((currentSelectionY - halfOffset) / (self.frame.size.height - offset)))
 		value = minValue + Int(percent * CGFloat(maxValue - minValue))
 		
--- SwiftlySlider/ViewController.swift
+++ SwiftlySlider/ViewController.swift
@@ -22,26 +22,26 @@ class ViewController: UIViewController, SwiftlySliderDelegate {
 		super.viewDidLoad()
 		
 		volumeSlider.delegate = self
-		volumeSlider.bgColor = UIColor.whiteColor()
-		volumeSlider.direction = SwiftlySlider.PickerDirection.Horizontal
+		volumeSlider.bgColor = UIColor.white
+		volumeSlider.direction = SwiftlySlider.PickerDirection.horizontal
 		volumeSlider.minValue = 1
 		volumeSlider.maxValue = 20
 		volumeSlider.currentValue = 17
 		volumeSlider.sliderImage = UIImage(named: "Slider")
 		volumeSlider.sliderImageOffset = CGPoint(x: 0, y: -1)
 		volumeSlider.sliderSize = CGSize(width: 30, height: 15)
-		volumeSlider.bgColor = UIColor.yellowColor()
+		volumeSlider.bgColor = UIColor.yellow
 		volumeSlider.useNormalIndicator = true
 		volumeSlider.normalValue = 15
 		
 		verticalVolumeSlider.delegate = self
-		verticalVolumeSlider.direction = SwiftlySlider.PickerDirection.Vertical
+		verticalVolumeSlider.direction = SwiftlySlider.PickerDirection.vertical
 		verticalVolumeSlider.useNormalIndicator = false
 	}
 
 	// MARK: - SwiftlyVolumeSliderDelegate
 	
-	func swiftlySliderValueChanged(value: Int) {
+	func swiftlySliderValueChanged(_ value: Int) {
 		testLabel.text = "\(value)"
 	}
 	
